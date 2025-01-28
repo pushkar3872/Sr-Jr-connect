d0c1235d3e619de275c12d8cc3747572659b9249
Varad73 <varadsane2004@gmail.com>
2025-01-28 20:23:13 +0530

    check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="">
         hello world
-        <h2>Hello from pushkar and subham</h2>
+        <h2>Hello from pushkar and shubham and varad</h2>
         <h4>Hello from se-2</h4>
         <h5>just a change </h5>
       </div>
