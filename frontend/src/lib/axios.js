8852046aa7caaa39fbdd76384fdccff89ffd7f9b
shubhamshinde5356 <shindeshubham5356@gmail.com>
2025-04-21 04:14:43 +0530

    try again

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    window.location.hostname === "localhost"
-      ? "http://localhost:4005/api"
-      : `http://${import.meta.env.VITE_IP_ADDRESS}:4005/api`,
+  baseURL: import.meta.env.MODE === "production" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
