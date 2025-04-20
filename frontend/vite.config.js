c37aac0b9bcb09b992afa268ac26f5e91a3c5f64
shubhamshinde5356 <shindeshubham5356@gmail.com>
2025-04-21 03:00:25 +0530

    Refactor package.json scripts for improved build process and update vite.config.js server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',   // Allows access from other devices
-    port: 5173         // Change if needed
-  },
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
